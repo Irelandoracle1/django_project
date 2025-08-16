621a1332c6397ed2e2cb69825ab2ddef089fd902
Irelandoracle1 <100025415+Irelandoracle1@users.noreply.github.com>
2025-08-16 02:20:27 +0100

    created procfile for production, set debug to false deployinh to heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1u2fzgxdjrvle5m(k2cf0x^n88!dct&owm(1)jx@6#x+^vuuhf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
